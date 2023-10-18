c8dec145be076502d0d036c5f1319f9b926059ab
WyattStanley <rws24@uw.edu>
2023-10-18 13:16:05 -0700

    changed photos

--- js/main.js
+++ js/main.js
@@ -18,3 +18,58 @@ function responsive_control() {
     x.className = "topnav";
   }
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the images and insert them inside the modal - use their "alt" text as a caption
+var images = document.querySelectorAll("[id^='myImg']");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// Function to open the modal for a specific image
+function openModal(imageElement) {
+  modal.style.display = "block";
+  modalImg.src = imageElement.src;
+  captionText.innerHTML = imageElement.alt;
+}
+
+// Add click event listeners to each image
+images.forEach(function(image) {
+  image.onclick = function() {
+    openModal(this);
+  };
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+
+
+
+
+
